3960f134091e5393b77cf48d6a1f89d90d2efe9d
maxcielOliv <maxcieloliveira11@gmail.com>
2023-10-05 17:24:00 -0300

    manipulação de arrays

---
+++ arrays.js
@@ -0,0 +1,56 @@
+//let lista = ['ovo', 'farinha', 'leite', 'manteiga'];
+//let lista2 = ['pão', 'pizza', 'hambúrguer'];
+
+
+let lista = [2,9,40,53,26];
+let lista2 = [];
+
+//procurar itens na lista
+/*lista2 = lista.find(function(item) {
+    return (item == 26) ? true : false; 
+});*/
+
+//retornar indice
+lista2 = lista.findIndex(function(item) {
+    return (item == 26) ? true : false; 
+});
+
+/*lista2 = lista.filter(function(item){
+    return (item < 20) ? true : false; 
+});*/
+
+/*lista2 = lista.map(function(item){
+    return item * 2;
+});
+
+for (let i in lista) {
+    lista2.push(lista[i] *2);
+}*/
+
+//adiciona itens no array
+//lista.push('chocolate');
+
+//remover item do array
+//lista.splice(0,1);
+
+//juntar arrays
+//let res = lista.concat(lista2);
+
+//remove o primeiro item do array
+//lista.shift();
+
+//remove o último item do array
+//lista.pop();
+
+let res = lista2;
+
+//transforma array em string
+//let res = lista.join(' ');
+
+//ordernar array
+//lista.sort();
+
+//decrescente
+//lista.reverse();
+
+console.log(res);
--- number.js
+++ number.js
@@ -1,9 +1,16 @@
-let n = 10.456465464;
+//let n = 10.456465464;
 //transformar em string
 //let res = n.toString();
 
 //determinar casas decimais
-let res = n.toFixed(2);
+//let res = n.toFixed(2);
 
+//let n = '50';
+//transformar string em numero inteiro
+//let res = parseInt(n) + 5;
+
+let n = '50.33';
+//transformar string em numero real
+let res = parseFloat(n) + 5;
 
 console.log(res);

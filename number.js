bb52bf04e4f40ba830ec2711482257426869f8fb
maxcielOliv <maxcieloliveira11@gmail.com>
2023-10-05 15:33:55 -0300

    first commit

---
+++ number.js
@@ -0,0 +1,9 @@
+let n = 10.456465464;
+//transformar em string
+//let res = n.toString();
+
+//determinar casas decimais
+let res = n.toFixed(2);
+
+
+console.log(res);
---
+++ strings.js
@@ -0,0 +1,36 @@
+//tamanho da váriavel
+let nome = 'maxciel oliveira de freitas';
+//console.log(nome.length);
+
+//achar posição
+//let resultado = nome.indexOf('freitas');
+
+//recorta string
+//let resultado = nome.slice(-7);
+//let resultado = nome.substring(0,7);
+
+//console.log(resultado);
+
+//substituir
+//let sub = nome.replace('freitas','melo');
+//console.log(sub);
+
+//deixar maiusculo
+/*let nomeMai = nome.toLocaleUpperCase();
+console.log(nomeMai);
+
+//deixar minusculo
+let nomeMin = nome.toLocaleLowerCase();
+console.log(nomeMin);*/
+
+//remover espaços do começo e fim
+//let remover = nome.trim();
+//console.log(remover);
+
+//achar caractere
+//let achar = nome.charAt(2);
+//console.log(achar);
+
+//
+let array = nome.split(' ');
+console.log(array);
